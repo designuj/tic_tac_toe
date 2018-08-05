e09b0492ec69a530f8940479709e8cb75c62c22e
designuj <designuj@fejm.pl>
2018-08-05 16:49:26 +0200

    Changed win logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,7 @@ public class App {
             System.out.println("Player '" + player + "' - make a move (input in one-digit-style):");
 
             board.move(player, scanner.nextInt()-1);
+
             if (player == 'x') {
                 player = 'o';
             } else {
@@ -19,7 +20,7 @@ public class App {
         }
 
         System.out.println(board);
-        if (board.getWinner() == '!') {
+        if (board.getWinner() == '·') {
             System.out.println("It's draw!");
         } else {
             System.out.println(board.getWinner() + " wins!");
--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,13 +3,39 @@ public class Board {
 
     }
 
-    public boolean finishGame() {
-
-        return true;
+    public boolean finishGame(char[][] board) {
+        boolean gameOver = true;
+        for (char[] inside:board) {
+            for (char deepInside:inside) {
+                if (deepInside == ' ') {
+                    gameOver = false;
+                    break;
+                }
+            }
+        }
+        if (gameOver) {
+            return true;
+        } else {
+            return getWinner() != '·';
+        }
     }
 
-    public char getWinner() {
+    public char getWinner(char[][] board) {
+        int[][] wins = {
+                            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //rows
+                            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //columns
+                            {0, 4, 8}, {2, 4, 6}             // diagonals
+        };
 
-        return ' ';
+        for (int[] field:wins) {
+            if (isWin(field[0], field[1], field[2])) {
+                return board[field[0]/3][field[0]%3];
+            }
+        }
+        return '·';
+    }
+
+    private boolean isWin(int fieldOne, int fieldTwo, int fieldThree) {
+        return
     }
 }
