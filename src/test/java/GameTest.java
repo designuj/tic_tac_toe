3312a662017913139f592f8216da594887039c05
designuj <designuj@fejm.pl>
2018-08-08 12:35:16 +0200

    Added more tests

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -6,7 +6,10 @@ public class GameTest {
 
     @Before
     public void initialisation() {
-
+        //each board chars:
+        //  0  4  8
+        // 10 14 18
+        // 20 24 28
     }
 
 
@@ -23,17 +26,29 @@ public class GameTest {
     }
 
     @Test
-    public void testMoreMoves() throws FullBoardException, LocationNotAvailableException {
+    public void testWinMoves() throws FullBoardException, LocationNotAvailableException {
         //Given
         Game game = new Game();
 
         //When
-        game.move('x', 4);
-        game.move('o', 0);
+        game.move('x', 0);
+        game.move('o', 4);
         game.move('x', 1);
-        game.move('o', 3);
+        game.move('o', 4);
+        game.move('x', 2);
 
         //Then
+        boolean resultOfBoard =
+                        game.getBoardArray()[1][1].toString().charAt(0) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(4) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(8) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(10) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(14) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(18) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(20) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(24) == ('x') &&
+                        game.getBoardArray()[1][1].toString().charAt(28) == ('x');
 
+        Assert.assertEquals(true, resultOfBoard);
     }
 }
